f0edce2dd83ac599ff10c35ef407df39c47d1691
SpairCode <1598111569@qq.com>
2020-08-28 17:47:47 +0800

    test

--- usb.js
+++ usb.js
@@ -1,9 +1,31 @@
 const usb = require('usb')
 
-usb.on('attach', function(device) { 
+usb.on('attach', function (device) {
     console.log(device)
 });
 
-usb.on('detach', function(device) { 
+usb.on('detach', function (device) {
     console.log(device)
+});
+
+
+var book = {};
+Object.defineProperties(book, {
+    _year: {
+        value: 2004
+    },
+    edition: {
+        value: 1
+    },
+    year: {
+        get: function () {
+            return this._year;
+        },
+        set: function (newValue) {
+            if (newValue > 2004) {
+                this._year = newValue;
+                this.edition += newValue - 2004;
+            }
+        }
+    }
 });
